90c3d59f28a4f6416ed789be9c90ac8f76e0d3fc
KimS112022 <Kimberley.Snow@Keyin.com>
2023-07-24 23:16:15 -0230

    made updates and fixed bugs, created comicsDAL.js

--- comicsRouter.js
+++ comicsRouter.js
@@ -1,137 +1,80 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("./config"); //Import the PostgreSQL conncection pool
+const comicsDAL = require("./comicsDAL");
 
 //Endpoint to fetch all comics
-router.get("/comics", async (req, res) => {
+router.get("/api/comics", async (req, res) => {
   try {
-    const queryResult = await pool.query("SELECT * FROM comics");
-    res.json(queryResult.rows);
-  } catch (err) {
-    console.error("Error fetching comics: ", err);
+    const comics = await comicsDAL.getAllComics(req.query);
+    res.json(comics);
+  } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
 //Endpoint to fetch a single comic by id
-router.get("/comics/:id", async (req, res) => {
-  const comic_id = req.params.id;
-
+router.get("/api/comics/:id", async (req, res) => {
   try {
-    const queryResult = await pool.query("SELECT * FROM comics WHERE id = $1", [
-      comic_id,
-    ]);
-    if (queryResult.rowCount === 0) {
-      res.status(404).json({ error: "Comic not found" });
+    const comic = await comicsDAL.getComicById(req.params.id);
+    if (comic) {
+      res.json(comic);
     } else {
-      res.json(queryResult.rows[0]);
+      res.status(404).json({ error: "Comic not found" });
     }
-  } catch (err) {
-    console.error("Error fetching comic by id:", err);
-    res.status(500).json({ error: "Internal server error" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //Endpoint to add a new comic
-router.post("comics", async (req, res) => {
-  const { title, issue, release_date, artist, price, description, quantity } =
-    req.body;
-
+router.post("/api/comics", async (req, res) => {
   try {
-    const queryResult = await pool.query(
-      "INSERT INTO comics (title, issue, release_date, artist, price, description, quantity) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-      [title, issue, release_date, artist, price, description, quantity]
-    );
-
-    res.status(201).json(queryResult.rows[0]);
-  } catch (err) {
-    console.error("Error adding comic: ", err);
-    res.status(500).json({ error: "Internal server error" });
+    const comic = await comicsDAL.createComic(req.body);
+    res.status(201).json(comic);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //Endpoint to update a comic by id
-router.put("/comics/:id", async (req, res) => {
-  const comic_id = req.params.id;
-  const { title, issue, release_date, artist, price, description, quantity } =
-    req.body;
-
+router.put("/api/comics/:id", async (req, res) => {
   try {
-    const queryResult = await pool.query(
-      "UPDATE comics SET title = $1, issue = $2, release_date = $3, artist = $4, price = $5, description = $6, quantity = $7 WHERE id = $8 RETURNING *",
-      [
-        title,
-        issue,
-        release_date,
-        artist,
-        price,
-        description,
-        quantity,
-        comic_id,
-      ]
-    );
-
-    if (queryResult.rowCount === 0) {
-      res.status(404).json({ error: "Comic not found" });
+    const comic = await comicsDAL.updateComic(req.params.id, req.body);
+    if (comic) {
+      res.json(comic);
     } else {
-      res.json(queryResult.rows[0]);
+      res.status(404).json({ error: "Comic not found" });
     }
-  } catch (err) {
-    console.error("Error updating comic: ", err);
-    res.status(500).json({ error: "Internal server error" });
+  } catch {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //Endpoint to update a comic's details by id using PATCH request
-router.patch("/comics/:id", async (req, res) => {
-  const comic_id = req.params.id;
-  const { title, issue, release_date, artist, price, description, quantity } =
-    req.body;
-
+router.patch("/api/comics/:id", async (req, res) => {
   try {
-    const queryResult = await pool.query(
-      "UPDATE comics SET title = $1, issue = $2, release_date = $3, artist = $4, price = $5, description = $6, quantity = $7 WHERE id = $8 RETURNING *",
-      [
-        title,
-        issue,
-        release_date,
-        artist,
-        price,
-        description,
-        quantity,
-        comic_id,
-      ]
-    );
-
-    if (queryResult.rowCount === 0) {
-      res.status(404).json({ error: "Comic not found" });
+    const comic = await comicsDAL.updateComic(req.params.id, req.body);
+    if (comic) {
+      res.json(comic);
     } else {
-      res.json(queryResult.rows[0]);
+      res.status(404).json({ error: "Comic not found" });
     }
-  } catch (err) {
-    console.error("Error updating comic:", err);
-    res.status(500).json({ error: "Internal server error" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //Endpoint to delete a comic by id
-router.delete("/comics/:id", async (req, res) => {
-  const comic_id = req.params.id;
-
+router.delete("/api/comics/:id", async (req, res) => {
   try {
-    const queryResult = await pool.query(
-      "DELETE FROM comics WHERE id = $1 RETURNING *",
-      [comic_id]
-    );
-
-    if (queryResult.rowCount === 0) {
-      res.status(404).json({ error: "Comic not found " });
-    } else {
+    const deletedComic = await comicsDAL.deleteComic(req.params.id);
+    if (deletedComic) {
       res.json({ message: "Comic deleted successfully" });
+    } else {
+      res.status(404).json({ error: "Comic not found" });
     }
-  } catch (err) {
-    console.error("Error deleting comic: ", err);
-    res.status(500).json({ error: "Internal server error" });
+  } catch {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
